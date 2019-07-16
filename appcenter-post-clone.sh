0e1ee3db2fbf41de45c53a9f6f5b4e5b992b1852
Hui <somnuslee@cmbchina.com>
2019-07-16 17:27:53 +0800

    update post-clone script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -7,6 +7,8 @@ echo "**************************************************************************
 /usr/bin/security cms -D -i $APPCENTER_SOURCE_DIRECTORY"/wildcard_dev_profile.mobileprovision" > tmp.plist
 uuid=`/usr/libexec/PlistBuddy -c 'Print:UUID' tmp.plist`
 echo "$uuid"
+echo "${APPCENTER_SOURCE_DIRECTORY}/wildcard_dev_profile.mobileprovision"
+echo "/Users/vsts/Library/MobileDevice/Provisioning Profiles/${uuid}.mobileprovision"
 cp -f $APPCENTER_SOURCE_DIRECTORY"/wildcard_dev_profile.mobileprovision" "/Users/vsts/Library/MobileDevice/Provisioning Profiles/"$uuid".mobileprovision"
 rm -rf tmp.plist
 
