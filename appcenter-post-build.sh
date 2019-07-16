44f2f6b8ad9b6e1e2d5016d8c1b8daf39f79018e
Hui <somnuslee@cmbchina.com>
2019-07-16 10:24:34 +0800

    update post-build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -27,7 +27,7 @@ echo ""
 
 echo "> Run UI test command"
 # Note: must put a space after each parameter/value pair
-appcenter test run xcuitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/sampleapp-ios-swift.ipa --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/DerivedData/Build/Products/Debug-iphoneos --token $appCenterLoginApiToken 
+appcenter test run xcuitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/sampleapp-ios-swift.ipa --test-series $testSeriesName --locale "en_US" --token $appCenterLoginApiToken 
 
 echo "**************************************************************************************************"
 echo "Post Build Script complete"
